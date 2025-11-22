9f80edbfdf3fe82879d082509f10c993ae981ff1
Esteban-jv <ejv.developer@gmail.com>
2025-11-22 17:08:23 -0500

    Separing the navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+  import { RouterLink, RouterView } from 'vue-router'
+  import NavBar from '@/shared/components/NavBar.vue'
 </script>
 
 <template>
   <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-      <RouterLink to="/counter">Counter</RouterLink>
-    </nav>
+    <NavBar />
   </header>
 
   <RouterView />
---
+++ src/shared/components/NavBar.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+    import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+    <nav>
+        <img src="https://th.bing.com/th/id/R.ddd5084dff8d7606019645f62c1712c6?rik=RJm8gtkd41am8Q&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fpokemon-logo-png-pokemon-logo-png-2000.png&ehk=%2bFdpvqx7Vk1jcWsSaxVIqArQSByK89fRtNQoRFktAhY%3d&risl=&pid=ImgRaw&r=0" alt="Pokemon app logo" width="100" height="40">
+        <span>App</span>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/counter">Counter</RouterLink>
+    </nav>
+</template>
+
+<style scoped>
+    nav {
+        display: flex;
+        align-items: flex-start;
+        gap: 1rem;
+        padding: 0 1rem;
+        width: 100%;
+    }
+
+    img {
+        margin-right: 1rem;
+    }
+
+    span {
+        margin-right: 1rem;
+    }
+
+    a {
+        margin-right: 1rem;
+    }
+
+    nav a.router-link-exact-active {
+        font-weight: bold;
+        text-decoration: underline;
+    }
+
+    nav a.router-link-exact-active:hover {
+        background-color: transparent;
+        color: inherit;
+    }
+
+    nav a {
+        display: inline-block;
+        padding: 0 1rem;
+        border-left: 1px solid #ccc;
+    }
+</style>
