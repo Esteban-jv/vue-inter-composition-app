510fd30a607c893c9c2c93ddfbdb75d28ab729f5
Esteban-jv <ejv.developer@gmail.com>
2025-11-24 21:44:32 -0500

    TankStack vue query

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { createApp } from 'vue'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+// app.use(VueQueryPlugin)
+VueQueryPlugin.install(app, {
+    queryClientConfig: {
+        defaultOptions: {
+            queries: {
+                cacheTime: 1000 * 120, // 2 minutes
+                refetchOnReconnect: 'always', // Si se pierde la conexion, volver a intentar
+            },
+        },
+    },
+})
 
 app.mount('#app')
